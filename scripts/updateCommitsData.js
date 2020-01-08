c66cc39f47c092ff27608a8288d4ecacfe90aee6
mirefly <mirefly@gmail.com>
2020-01-08 12:15:26 -0700

    Fetch more than 30 commits (one page) from API

--- scripts/updateCommitsData.js
+++ scripts/updateCommitsData.js
@@ -2,12 +2,16 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
+const queryString = require('query-string');
 
-const URL_COMMITS = 'https://api.github.com/search/commits';
+const URL_COMMITS = 'https://api.github.com/search/commits?';
 const OUTPUT_PATH = path.resolve(__dirname, '../data/commits.json');
 
 function makeQuery(repo) {
-  return `?q=repo:${repo}+author:mirefly`;
+  return queryString.stringify(
+    {q: `repo:${repo}+author:mirefly`},
+    {encode: false},
+  );
 }
 
 repos = [
@@ -17,8 +21,10 @@ repos = [
   'mozilla/application-services',
 ];
 
-async function fetchCommits(query) {
-  const resp = await fetch(URL_COMMITS + query, {
+async function fetchCommitsOnePage(url) {
+  console.log(`fetching commits at ${url}`);
+
+  const resp = await fetch(url, {
     method: 'GET',
     headers: {
       Accept: 'application/vnd.github.cloak-preview',
@@ -26,21 +32,38 @@ async function fetchCommits(query) {
   });
 
   if (!resp.ok) {
-    throw Error('The Response is not OK');
+    throw Error(`The Response is not OK: ${resp.status} at ${url} `);
   }
 
   let data = await resp.json();
 
-  data = data.items.map(v => {
-    return {
-      sha: v.sha,
-      htmlUrl: v.html_url,
-      date: v.commit.author.date,
-      message: v.commit.message,
-    };
-  });
+  data = data.items.map(v => ({
+    sha: v.sha,
+    htmlUrl: v.html_url,
+    date: v.commit.author.date,
+    message: v.commit.message,
+  }));
 
-  return data;
+  const linksString = resp.headers.get('link');
+  return {data, next: linksString && getNextUrl(linksString)};
+}
+
+const getNextUrl = linksString => {
+  const links = linksString.split(',');
+  const nextLink = links.filter(link => link.includes('rel="next"'));
+
+  return nextLink.length > 0 ? nextLink[0].match(/\<(.*?)\>/)[1] : null;
+};
+
+async function fetchCommits(url) {
+  const {data, next} = await fetchCommitsOnePage(url);
+  let newData = [];
+
+  if (next) {
+    newData = await fetchCommits(next);
+  }
+
+  return data.concat(newData);
 }
 
 async function run() {
@@ -48,7 +71,7 @@ async function run() {
 
   try {
     for (const repo of repos) {
-      const data = await fetchCommits(makeQuery(repo));
+      const data = await fetchCommits(URL_COMMITS + makeQuery(repo));
       rv[repo] = data.sort(function(a, b) {
         return a.date < b.date ? 1 : -1;
       });
@@ -63,4 +86,10 @@ async function run() {
   }
 }
 
-run();
+module.exports = {
+  getNextUrl,
+};
+
+if (require.main === module) {
+  run();
+}
